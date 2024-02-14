606bd386138baff2e8258e663427f7ea2c9db185
hita-fttk <fttkmcc77@gmail.com>
2024-02-14 13:45:39 +0900

    higaki-ex08

---
+++ src/main/java/copilot/exercises/ex08/p01/Student.java
@@ -0,0 +1,18 @@
+package copilot.exercises.ex08.p01;
+
+public class Student {
+    private Integer studentId;
+    private String studentName;
+    public Integer getStudentId() {
+        return studentId;
+    }
+    public void setStudentId(Integer studentId) {
+        this.studentId = studentId;
+    }
+    public String getStudentName() {
+        return studentName;
+    }
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+}
